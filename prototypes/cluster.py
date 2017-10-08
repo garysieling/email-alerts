fdb7248045524d63a296418d78fe07ebef55ca61
Ubuntu <gary@Solr.kidebaugf51eblpuxo5ngbkr0e.bx.internal.cloudapp.net>
2017-10-08 21:10:24 +0000

    fixes

--- prototypes/cluster.py
+++ prototypes/cluster.py
@@ -2,20 +2,23 @@ from annoy import AnnoyIndex
 
 file = "/uploads/pathToSaveModel1.txt"
 
-content = []
-with open(file) as f:
-  content = f.readlines()
+#content = []
+#with open(file) as f:
+#  content = f.readlines()
 
-content = [x.split(" ") for x in content]
+#content = [x.split(" ") for x in content]
+from gensim.models.keyedvectors import KeyedVectors
+import gensim
+
+model = gensim.models.Word2Vec.load(file)
 
 t = AnnoyIndex(50) 
-idx = 0
 terms = [i[0] for i in content]
 termMapping = []
 for i in content:
   vec = [float(a) for a in i[1:]]
   print(len(vec))
-  t.add_item(len(idx), vec)
+  t.add_item(len(termMapping), vec)
 
   termMapping.append(i[0])
 
@@ -26,6 +29,8 @@ u = AnnoyIndex(50)
 u.load('test.ann')
 
 for index, word in termMapping:
+  print(index)
+  print(word)
   near = u.get_nns_by_item(index, 10) # nearest 10 terms
   nearWords = [terms[i] for i in near]
   print(word + ": " + nearWords)
