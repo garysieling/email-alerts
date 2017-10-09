7103021e8e7617b64f5a80ad660bc32f3c8e0582
Ubuntu <gary@Solr.kidebaugf51eblpuxo5ngbkr0e.bx.internal.cloudapp.net>
2017-10-09 00:56:46 +0000

    clustering

--- prototypes/cluster.py
+++ prototypes/cluster.py
@@ -1,36 +1,45 @@
 from annoy import AnnoyIndex
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-file = "/uploads/pathToSaveModel1.txt"
+
+file = "/uploads/en_1000_no_stem/en.model"
 
 #content = []
 #with open(file) as f:
 #  content = f.readlines()
 
 #content = [x.split(" ") for x in content]
-from gensim.models.keyedvectors import KeyedVectors
+#from gensim.models.keyedvectors import KeyedVectors
 import gensim
 
 model = gensim.models.Word2Vec.load(file)
+#model = gensim.models.KeyedVectors.load_word2vec_format(file, binary=True)
 
-t = AnnoyIndex(50) 
-terms = [i[0] for i in content]
-termMapping = []
-for i in content:
-  vec = [float(a) for a in i[1:]]
-  print(len(vec))
-  t.add_item(len(termMapping), vec)
+model.vector_size = 1000
+from gensim.similarities.index import AnnoyIndexer
+# 100 trees are being used in this example
+annoy_index = AnnoyIndexer(model,100)
 
-  termMapping.append(i[0])
 
-t.build(10) # 10 trees
-t.save('test.ann')
+fname = 'index.ann'
 
-u = AnnoyIndex(50)
-u.load('test.ann')
+# Persist index to disk
+annoy_index.save(fname)
 
-for index, word in termMapping:
-  print(index)
-  print(word)
-  near = u.get_nns_by_item(index, 10) # nearest 10 terms
-  nearWords = [terms[i] for i in near]
-  print(word + ": " + nearWords)
+# Load index back
+#if os.path.exists(fname):
+#   annoy_index2 = AnnoyIndexer()
+#   annoy_index2.load(fname)
+#   annoy_index2.model = model
+#
+
+# Derive the vector for the word "army" in our model
+#vector = model["science"]
+# The instance of AnnoyIndexer we just created is passed 
+#approximate_neighbors = model.most_similar([vector], topn=5, indexer=annoy_index)
+# Neatly print the approximate_neighbors and their corresponding cosine similarity values
+#for neighbor in approximate_neighbors:
+#  print(neighbor)
+
+
