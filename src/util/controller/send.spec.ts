d7437b8ff4a18569b8bbd28fd63feeb77f7ed1e0
gary <gary@garysieling.com>
2017-12-14 08:26:51 -0500

    Add a couple more tests

--- src/util/controller/send.spec.ts
+++ src/util/controller/send.spec.ts
@@ -10,3 +10,21 @@ describe('sends one email per id', function() {
     assert.equal(true, false);
   });
 });
+
+
+describe('does not send the same id twice across emails', function() {
+  it('sends one email per id', function() {
+    assert.equal(true, false);
+  });
+});
+
+
+describe('sends newsletter content to people with no requests', function() {
+  it('sends first email when someone signs up', function() {
+    assert.equal(true, false);
+  });
+
+  it('sends second email a week later', function() {
+    assert.equal(true, false);
+  });
+});
