dd5a9f74aa43f1aed038acfcd151d9c31229b761
gary <gary@garysieling.com>
2017-12-14 08:28:11 -0500

    more test definition

--- src/util/controller/send.spec.ts
+++ src/util/controller/send.spec.ts
@@ -12,8 +12,12 @@ describe('sends one email per id', function() {
 });
 
 
-describe('does not send the same id twice across emails', function() {
-  it('sends one email per id', function() {
+describe('includes the right things', function() {
+  it('does not send the same id twice across emails', function() {
+    assert.equal(true, false);
+  });
+
+  it('includes ad block in machine generated emails', function() {
     assert.equal(true, false);
   });
 });
@@ -25,6 +29,12 @@ describe('sends newsletter content to people with no requests', function() {
   });
 
   it('sends second email a week later', function() {
+    // loop over every newsletter / index
+    assert.equal(true, false);
+  });
+
+  it('includes ad block in all newsletter emails', function() {
+    // loops over every newsletter
     assert.equal(true, false);
   });
 });
